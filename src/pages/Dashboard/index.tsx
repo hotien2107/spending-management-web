85f1251b9594f0d9a3f660f05573f6d71aa872b0
193499-Hồ Hoàng Việt Tiến <tien.hohoangviet@thegioididong.com>
2023-08-10 21:43:24 +0700

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+
+const Home = () => {
+  return (
+    <div>
+      Home <Button />
+    </div>
+  );
+};
+
+export default Home;
